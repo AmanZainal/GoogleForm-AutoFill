1cb000994b6fd0e3f8fa0aee4bf3ab6601e2f94c
AmanZainal <69044708+AmanZainal@users.noreply.github.com>
2021-08-01 12:42:19 +0800

    AutoFill Daily Temperature Recording
    automated daily temperature recording in lieu of covid-19

--- AutoFill.py
+++ AutoFill.py
@@ -6,58 +6,64 @@ import time
 import sys
 
 # URL to the form you want to fill. formResponse should be used instead of viewform
-url = 'https://docs.google.com/forms/d/e/1FAIpQLScDT-xtsF9eViroPOgaSkjayQTrVwk8H8An-i2nH5JbX8F-PQ/formResponse'
+testurl = 'https://docs.google.com/forms/d/e/1FAIpQLSekPW7KZ9EsjDsBJ2ox7OwMBGhcErhSvMx1ml3bjZWjmHx8vQ/formResponse'
+actualURL = 'https://docs.google.com/forms/d/e/1FAIpQLSdmqeLLjFjYtKGxX2cwSiQ34iQhJ4ajcUbd0gVjhU3_dQidug/formResponse'
 
 
-def get_values():
-    """It returns a list of different form data to be submitted by send_attendance method.
-    subjects_time is a dictionary with Day as keys and time and subjects in a list as values.
-    value_list is a list of lectures' subject and time of current_day."""
 
     values_list = []
-    now = datetime.datetime.now()
-    day_name = now.strftime("%A")
-
-    subjects_time = {
-        "Monday": [["10", "30", "AJ(HC)"], ["11", "30", "DCDR(PC)"], ["1", "00", "WT(AM)"], ["2", "00", "SE(KD)"]],
-        "Tuesday": [["11", "30", "SE(KD)"], ["1", "00", "AJ(HC)"]],
-        "Wednesday": [["11", "30", ".NET(PC)"], ["1", "00", "SE(KD)"], ["2", "00", ".NET(PC)"]],
-        "Thursday": [["10", "30", "WT(AM)"], ["11", "30", "DCDR(PC)"], ["1", "00", "AJ(HC)"]],
-        "Friday": [["10", "30", "AJ(HC)"], ["11", "30", ".NET(PC)"], ["1", "00", "SE(KD)"], ["2", "00", ".NET(PC)"]],
-    }
-
-    date = str(now).split('-')
-
-    for i in subjects_time[day_name]:
-        '''keys are the value of 'name' element of the '''
+        """keys are the value of 'name' element in the form POST""" "these values are for ACTUAL form"
+        
+        """values = {
+            #NAME
+            "entry.639865078": yourNameHere
+            #BLOCK
+            "entry.1883478312": yourBlockHere
+            #Level
+            "entry.507729214": yourLevelHere
+            #Room
+            "entry.1691923912": yourRoomHere
+            #Bed number
+            "entry.2005248198": yourNumberHere
+            #Temperature status
+            "entry.1920766654": Normal / Not Normal
+            
+            #idk what these values are but theyre listed in the post request 
+            "entry.1920766654_sentinel": 
+            "fvv": 1
+            "draftResponse": [null,null,"3241614246892369303"]
+            "pageHistory": 0
+            "fbzx": 3241614246892369303
+        }"""
+        
+        
+        "TEST FORM at TEST URL"
         values = {
-            # Email Address
-            "emailAddress": str(sys.argv[1]),
-            # Enrollment Number
-            "entry.33987362": str(sys.argv[2]),
-            # Course
-            "entry.363926033": "BE",
-            # Branch
-            "entry.733518766": "IT",
-            # Semester
-            "entry.114626584": "Sem-6",
-            # Subject
-            "entry.609979780": i[2],
-            # Date
-            "entry.1916623197_year": date[0],
-            "entry.1916623197_month": date[1],
-            "entry.1916623197_day": date[2][0:2],
-            # Time
-            "entry.125609755_hour": i[0],
-            "entry.125609755_minute": i[1],
+            #NAME
+            "entry.955312797": aman
+            #BLOCK
+            "entry.1835522495": Bayley
+            #Level
+            "entry.1444551311": 7
+            #Room
+            "entry.1317176630": 14
+            #Bed number
+            "entry.374108602": A
+            #Temperature status
+            "entry.364510544": Normal
+            
+            #default values
+            "entry.364510544_sentinel": 
+            "fvv": 1
+            "draftResponse": [null,null,"-2892881215578928413"]
+            "pageHistory": 0
+            "fbzx": -2892881215578928413
         }
-
-        values_list.append(values)
-
-    return values_list
+        
+        values_list = values_list.append(values)
 
 
-def send_attendance(url, data):
+def send_temperature(testurl, data):
     """It takes google form url which is to be submitted and also data which is a list of data to be submitted in the form iteratively."""
 
     for d in data:
@@ -68,7 +74,4 @@ def send_attendance(url, data):
         except:
             print("Error Occured!")
 
-
-final_data = get_values()
-
-send_attendance(url, final_data)
+send_temperature(testurl, values_list)
